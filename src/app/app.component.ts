8c10da90c592820da7bfc38810e0d82421ffd8b4
Hugues Labat <labat.hugues@gmail.com>
2024-05-22 14:39:42 +0200

    delete main tabs & add main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ToolComponent } from './tool/tool.component';
-import { MainTabsComponent } from './main-tabs/main-tabs.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,ToolComponent,MainTabsComponent],
+  imports: [RouterOutlet,HeaderComponent,ToolComponent,MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ToolsListComponent } from './tools-list/tools-list.component';
+import { ProjetcsComponent } from './projetcs/projetcs.component';
 
 export const routes: Routes = [
-    {path: 'tools', component: ToolsListComponent}
+    {path: '', component: ToolsListComponent},
+    {path: 'projects', component: ProjetcsComponent}
 ];
