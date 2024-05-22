7a57589fa77c794b3392f79886c8601d6b7bda1e
Hugues Labat <labat.hugues@gmail.com>
2024-05-22 15:53:19 +0200

    create subscription form

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-subscription',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './subscription.component.html',
   styleUrl: './subscription.component.scss'
 })
 export class SubscriptionComponent {
+  userEmail!: string;
+  userPassword!: string;
+  userConfirmedPassword!: string;
 
+  ngOnInit(): void {
+
+  }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+  }
 }
