50e77c5390b98b3fd837ef7abbd9dc01db121a61
Hugues Labat <labat.hugues@gmail.com>
2024-05-22 14:52:08 +0200

    complete main menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
