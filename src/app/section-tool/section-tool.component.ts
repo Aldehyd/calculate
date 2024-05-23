5607804baf4aee771d373f640905b851e9489a52
Hugues Labat <labat.hugues@gmail.com>
2024-05-23 08:19:36 +0200

    create section tool first form

--- src/app/section-tool/section-tool.component.ts
+++ src/app/section-tool/section-tool.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 @Component({
   selector: 'app-section-tool',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './section-tool.component.html',
   styleUrl: './section-tool.component.scss'
 })
 export class SectionToolComponent {
+  projectName!: string;
 
+  ngOnInit(): void {
+
+  }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+  }
 }
