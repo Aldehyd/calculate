bfaa513e4a0f002aaf45f4e34d38ad47eca9729f
Hugues Labat <labat.hugues@gmail.com>
2024-05-30 13:58:16 +0200

    add remove point button to section tool geometry

--- src/app/point-form-line/point-form-line.component.ts
+++ src/app/point-form-line/point-form-line.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PointLineForm } from '../models/point-line-form.model';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-point-form-line',
   standalone: true,
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './point-form-line.component.html',
   styleUrl: './point-form-line.component.scss'
 })
@@ -13,6 +14,7 @@ export class PointFormLineComponent implements OnInit {
 
   @Input() point!: PointLineForm;
   @Input() formGroup!: FormGroup;
+  @Input() geometry!: any;
 
   formControlNameX!: string;
   formControlNameY!: string;
@@ -20,5 +22,9 @@ export class PointFormLineComponent implements OnInit {
   ngOnInit(): void {
     this.formControlNameX = `point${this.point.indice}X`;
     this.formControlNameY = `point${this.point.indice}Y`;
+  }
+
+  removePoint(): void {
+    this.geometry.splice(this.point.indice,1);
   }
 }
