137ebf4e2565d089871ed481770c2de972395f8d
Hugues Labat <labat.hugues@gmail.com>
2024-06-11 14:56:42 +0200

    remove wind from tools list

--- src/app/tools-list/tools-list.component.ts
+++ src/app/tools-list/tools-list.component.ts
@@ -21,13 +21,6 @@ export class ToolsListComponent implements OnInit{
         url: "section-tool",
         description: "Calcul des propriétés d'une section à parois minces avec ou sans raidisseur.",
         norm: "Norme: EN NF 1993 1-3"
-      },
-      {
-        id: 1,
-        title: "Vent",
-        url: "wind",
-        description: "Calcul du vent.",
-        norm: "Norme: EN NF 1991 1-1"
       }
     ]
   }
