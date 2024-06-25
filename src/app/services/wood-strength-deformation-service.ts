6ccb39b72d14a3ae40bb5c3785e59633cd99b7ec
Hugues Labat <labat.hugues@gmail.com>
2024-06-25 11:29:56 +0200

    add interface to wood strength deformation service

--- src/app/services/wood-strength-deformation-service.ts
+++ src/app/services/wood-strength-deformation-service.ts
@@ -1,12 +1,18 @@
 import { Injectable, OnInit } from "@angular/core";
 
+interface projectDetailsInterface {
+    materiau: string,
+    type: string | null,
+    classeService: string,
+    action: string
+}
 @Injectable({
     providedIn: 'root'
 })
 export class woodStrengthDeformationService implements OnInit {
     projectName!: string;
     modifyProject!: boolean;
-
+    projectDetails!: projectDetailsInterface;
 
     ngOnInit(): void {
     }
