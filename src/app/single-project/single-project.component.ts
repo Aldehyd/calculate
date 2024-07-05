d8dc9c1f8ceb739f596fe159e38a7ea49568d512
Hugues Labat <labat.hugues@gmail.com>
2024-07-05 17:14:01 +0200

    make remove project functionality work

--- src/app/single-project/single-project.component.ts
+++ src/app/single-project/single-project.component.ts
@@ -28,7 +28,7 @@ export class SingleProjectComponent {
   ) {}
 
   removeProject(id: number): void {
-    this.removeProject$ = this.http.post('https://calculs-structure/app/remove_project',{mail: this.accountService.userEmail, id: id},{responseType: 'text'}).pipe(
+    this.removeProject$ = this.http.post('https://calculs-structure.fr/app/remove_project',{mail: this.accountService.userEmail, id: id},{responseType: 'text'}).pipe(
       tap(()=> {
         this.getProjects$ = this.accountService.getProjects(this.accountService.userEmail);
       })
