63623f0462b01386cd1a0c1b1c4185596e6f6776
Hugues Labat <labat.hugues@gmail.com>
2024-07-08 15:22:52 +0200

    complete column properties

--- src/app/column-tool-properties/column-tool-properties.component.ts
+++ src/app/column-tool-properties/column-tool-properties.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { columnService } from '../services/column.service';
 import { TipComponent } from '../tip/tip.component';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-column-tool-properties',
@@ -17,8 +18,12 @@ export class ColumnToolPropertiesComponent implements OnInit {
   projectName!: string;
   columnForm!: FormGroup;
   showN!: false | 'g' | 'q';
+  NColor!: string;
+  MColor!: string;
   showM!: false | 'g' | 'q';
   isFormValid!: boolean;
+
+  updateProperties$!: Observable<any>;
 
   constructor(
     private columnService: columnService,
@@ -31,6 +36,38 @@ export class ColumnToolPropertiesComponent implements OnInit {
     this.columnForm = this.formBuilder.group({Ng: [null], Mg: [null], Nq: [null], Mq: [null], M01M02: [null], expoClass: [null], fck: [null], steel: [null], length: [null], sectionLength: [null], sectionWidth: [null]});
     this.showN = false;
     this.showM = false;
+    this.isFormValid = false;
+  }
+
+  ngAfterViewInit(): void {
+    this.updateProperties$ = this.columnForm.valueChanges.pipe(
+      tap(formValues => {
+        if(formValues.length !== null && formValues.length > 0 && formValues.sectionLength !== null && formValues.sectionLength > 0 && formValues.sectionWidth !== null && formValues.sectionWidth > 0 && formValues.Ng !== null && formValues.Ng > 0 && formValues.Mg !== null && formValues.Mg > 0 && formValues.Nq !== null && formValues.Nq > 0 && formValues.Mq !== null && formValues.Mq > 0 && formValues.expoClass !== null && formValues.M01M02!== null && formValues.M01M02 > 0 && formValues.fck !== null && formValues.fck > 0 && formValues.steel !== null) {
+          this.isFormValid = true;
+        } else {
+          this.isFormValid = false;
+        };
+      })
+    );
+    this.updateProperties$.subscribe();
+  }
+
+  handleNView(type: false | 'g' | 'q'): void {
+    this.showN = type;
+    if(type === 'g') {
+      this.NColor = 'blue';
+    } else {
+      this.NColor = 'green';
+    };
+  }
+
+  handleMView(type: false | 'g' | 'q'): void {
+    this.showM = type;
+    if(type === 'g') {
+      this.MColor = 'blue';
+    } else {
+      this.MColor = 'green';
+    };
   }
 
   submitForm(): void {
