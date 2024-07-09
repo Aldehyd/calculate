a3cd510e641c726ec765f1d045c9013303d8f12b
Hugues Labat <labat.hugues@gmail.com>
2024-07-09 14:37:23 +0200

    progress on column tool steels

--- src/app/column-tool-steels/column-tool-steels.component.ts
+++ src/app/column-tool-steels/column-tool-steels.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { columnService } from '../services/column.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-column-tool-steels',
@@ -12,15 +13,20 @@ import { CommonModule } from '@angular/common';
 export class ColumnToolSteelsComponent implements OnInit {
 
   projectName!: string;
+  muCu!: number;
+  muBc!: number;
+  muLu!: number;
 
-  constructor(private columnService:columnService) {}
+  constructor(private columnService:columnService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.projectName = this.columnService.projectName;
   }
 
   onNextButtonClick():void {
-
+    this.router.navigateByUrl('/column-tool/steel-choice')
   }
 
 }
